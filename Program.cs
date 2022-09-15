a935d47e19ca4b193ee02600a4c8e45b4f3758de
mrvad <mrvad@DESKTOP-MIPOAPR>
2022-09-15 22:01:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Добро пожаловать, у вас есть 3 попытки. Введите 3 числа");
+int b = Convert.ToInt32(Console.ReadLine());
+Random x = new Random();
+int a = x.Next(-1, 51);
+if (a == b)
+{
+    Console.WriteLine("Поздравляем вы угадали число с 1 попытки!");
+}
+else
+{
+    Console.WriteLine("Неверно, попробуйте еще раз");
+}
+int z = Convert.ToInt32(Console.ReadLine());
+if (a == z)
+{
+    Console.WriteLine("Поздравляем вы угадали число со 2 попытки!");
+}
+else
+{
+    Console.WriteLine("Неверно, попробуйте еще раз");
+}
+int v = Convert.ToInt32(Console.ReadLine());
+if (a == v)
+{
+    Console.WriteLine("Поздравляем вы угадали число с 3 попытки!");
+}
+else
+{
+    Console.WriteLine("Вы не угадали число");
+}
+
+Console.WriteLine($"Загаданное число: {a}"); 
